3e1fad8283b18b2931c6e631fd61f44a8099b417
Lochmannweb <lochmannweb@gmail.com>
2025-08-25 20:16:30 +0200

    supabase test

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,9 +1,13 @@
 import { createClient } from '@supabase/supabase-js'
 
-const url = process.env.NEXT_PUBLIC_SUPABASE_URL
-const key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
-if (!url) throw new Error("Missing env: NEXT_PUBLIC_SUPABASE_URL")
-if (!key) throw new Error("Missing env: NEXT_PUBLIC_SUPABASE_ANON_KEY")
+if (!supabaseUrl) {
+  throw new Error("Missing env var: NEXT_PUBLIC_SUPABASE_URL")
+}
+if (!supabaseKey) {
+  throw new Error("Missing env var: NEXT_PUBLIC_SUPABASE_ANON_KEY")
+}
 
-export const supabase = createClient(url, key)
+export const supabase = createClient(supabaseUrl, supabaseKey)
