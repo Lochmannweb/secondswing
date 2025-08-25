de73ad9d864d0c4cdd5455064bdb68edd46f44c3
Lochmannweb <lochmannweb@gmail.com>
2025-08-25 19:57:25 +0200

    supabase

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -7,8 +7,8 @@ export const supabase = createClient(
 
 export function getSupabase() {
   if (!supabase) {
-    const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string
-    const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
+    const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL 
+    const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY 
 
     if (!supabaseUrl || !supabaseAnonKey) {
       throw new Error(
