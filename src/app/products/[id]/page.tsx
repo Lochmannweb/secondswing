166de60ce75c892b9c29f1ec14bdfbbf77996c7f
Lochmannweb <lochmannweb@gmail.com>
2025-08-27 13:22:59 +0200

    supabase test 2

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,10 +1,6 @@
+import { supabase } from "@/lib/supabaseClient"
 import { Box, Button } from "@mui/material"
-import { createClient } from "@supabase/supabase-js"
-
-const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-)
+// import createClient from "@supabase/supabase-js"
 
 interface ProductPageProps {
   params: { id: string }
