646d9400515ac3e5691190355be553d768c4c69a
Lochmannweb <lochmannweb@gmail.com>
2025-08-30 10:22:57 +0200

    chat placeholder

---
+++ src/app/chat/[receiver_id]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function chat() {
+  return (
+    <>
+        Chat
+    </>
+  )
+}
+
+export default chat
---
+++ src/app/chat/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function chatHistory() {
+  return (
+    <>
+        Chat History
+    </>
+  )
+}
+
+export default chatHistory
